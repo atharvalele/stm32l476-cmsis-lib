4ee9f7d350c87364d64cbc13fe9fa740194f01cd
Atharva Lele <itsatharva@gmail.com>
2021-01-15 08:36:08 +0530

    main: init usart and nvic

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #include "stm32l4xx.h"
 #include "device/gpio.h"
+#include "device/nvic.h"
 #include "device/rcc.h"
+#include "device/usart.h"
 
 void system_init(void)
 {
     rcc_config();
     gpio_config();
+    usart_config();
+    nvic_config();
 }
 
 int main(void)
