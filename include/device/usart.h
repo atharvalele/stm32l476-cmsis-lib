94bb79074250d8a02054f0cf6bd370fd0e32d3e6
Atharva Lele <itsatharva@gmail.com>
2021-01-31 10:04:03 +0530

    usart: refactor code a bit
    Introduce a struct to handle everything a port needs. Use the structure pointer
    in functions to reduce switch-case code to check which USART we want to access
    or send data to.
    
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

--- include/device/usart.h
+++ include/device/usart.h
@@ -14,32 +14,46 @@ extern struct sw_fifo_t usart1_tx_fifo;
 extern char usart1_tx_buf[USART_BUF_SIZE];
 extern struct sw_fifo_t usart1_rx_fifo;
 extern char usart1_rx_buf[USART_BUF_SIZE];
+extern struct usart_handle_t usart1;
 #endif
 #ifdef USART2_ENABLED
 extern struct sw_fifo_t usart2_tx_fifo;
 extern char usart2_tx_buf[USART_BUF_SIZE];
 extern struct sw_fifo_t usart2_rx_fifo;
 extern char usart2_rx_buf[USART_BUF_SIZE];
+extern struct usart_handle_t usart2;
 #endif
 #ifdef USART3_ENABLED
 extern struct sw_fifo_t usart3_tx_fifo;
 extern char usart3_tx_buf[USART_BUF_SIZE];
 extern struct sw_fifo_t usart3_rx_fifo;
 extern char usart3_rx_buf[USART_BUF_SIZE];
+extern struct usart_handle_t usart3;
 #endif
 #ifdef USART4_ENABLED
 extern struct sw_fifo_t usart4_tx_fifo;
 extern char usart4_tx_buf[USART_BUF_SIZE];
 extern struct sw_fifo_t usart4_rx_fifo;
 extern char usart4_rx_buf[USART_BUF_SIZE];
+extern struct usart_handle_t usart4;
 #endif
 #ifdef USART5_ENABLED
 extern struct sw_fifo_t usart5_tx_fifo;
 extern char usart5_tx_buf[USART_BUF_SIZE];
 extern struct sw_fifo_t usart5_rx_fifo;
 extern char usart5_rx_buf[USART_BUF_SIZE];
+extern struct usart_handle_t usart5;
 #endif
 
+struct usart_handle_t {
+    USART_TypeDef *port;
+    struct sw_fifo_t *tx_fifo;
+    struct sw_fifo_t *rx_fifo;
+    char *tx_buf;
+    char *rx_buf;
+    uint32_t baudrate;
+};
+
 void usart_config(void);
-void usart_setup(USART_TypeDef *usart, uint32_t baudrate);
-void usart_str_send(USART_TypeDef *usart, char *str);
+void usart_setup(struct usart_handle_t *usart);
+void usart_str_send(struct usart_handle_t *usart, char *str);
--- src/device/usart.c
+++ src/device/usart.c
@@ -13,30 +13,70 @@ struct sw_fifo_t usart1_tx_fifo;
 char usart1_tx_buf[USART_BUF_SIZE];
 struct sw_fifo_t usart1_rx_fifo;
 char usart1_rx_buf[USART_BUF_SIZE];
+struct usart_handle_t usart1 = {
+    .port = USART1,
+    .baudrate = 115200,
+    .tx_fifo = &usart1_tx_fifo,
+    .tx_buf = usart1_tx_buf,
+    .rx_fifo = &usart1_rx_fifo,
+    .rx_buf = usart1_rx_buf,
+};
 #endif
 #ifdef USART2_ENABLED
 struct sw_fifo_t usart2_tx_fifo;
 char usart2_tx_buf[USART_BUF_SIZE];
 struct sw_fifo_t usart2_rx_fifo;
 char usart2_rx_buf[USART_BUF_SIZE];
+struct usart_handle_t usart2 = {
+    .port = USART2,
+    .baudrate = 115200,
+    .tx_fifo = &usart2_tx_fifo,
+    .tx_buf = usart2_tx_buf,
+    .rx_fifo = &usart2_rx_fifo,
+    .rx_buf = usart2_rx_buf,
+};
 #endif
 #ifdef USART3_ENABLED
 struct sw_fifo_t usart3_tx_fifo;
 char usart3_tx_buf[USART_BUF_SIZE];
 struct sw_fifo_t usart3_rx_fifo;
 char usart3_rx_buf[USART_BUF_SIZE];
+struct usart_handle_t usart3 = {
+    .port = USART3,
+    .baudrate = 115200,
+    .tx_fifo = &usart3_tx_fifo,
+    .tx_buf = usart3_tx_buf,
+    .rx_fifo = &usart3_rx_fifo,
+    .rx_buf = usart3_rx_buf,
+};
 #endif
 #ifdef USART4_ENABLED
 struct sw_fifo_t usart4_tx_fifo;
 char usart4_tx_buf[USART_BUF_SIZE];
 struct sw_fifo_t usart4_rx_fifo;
 char usart4_rx_buf[USART_BUF_SIZE];
+struct usart_handle_t usart4 = {
+    .port = USART4,
+    .baudrate = 115200,
+    .tx_fifo = &usart4_tx_fifo,
+    .tx_buf = usart4_tx_buf,
+    .rx_fifo = &usart4_rx_fifo,
+    .rx_buf = usart4_rx_buf,
+};
 #endif
 #ifdef USART5_ENABLED
 struct sw_fifo_t usart5_tx_fifo;
 char usart5_tx_buf[USART_BUF_SIZE];
 struct sw_fifo_t usart5_rx_fifo;
 char usart5_rx_buf[USART_BUF_SIZE];
+struct usart_handle_t usart5 = {
+    .port = USART5,
+    .baudrate = 115200,
+    .tx_fifo = &usart5_tx_fifo,
+    .tx_buf = usart5_tx_buf,
+    .rx_fifo = &usart5_rx_fifo,
+    .rx_buf = usart5_rx_buf,
+};
 #endif
 
 /* Setup all USARTs */
@@ -44,67 +84,69 @@ void usart_config(void)
 {
 #ifdef USART1_ENABLED
     SET_BIT(RCC->APB2ENR1, RCC_APB2ENR_USART1EN);
-    usart_setup(USART1, 115200);
-    sw_fifo_init(&usart1_tx_fifo, usart1_tx_buf, USART_BUF_SIZE);
-    sw_fifo_init(&usart1_rx_fifo, usart1_rx_buf, USART_BUF_SIZE);
+    usart_setup(&usart1);
 #endif
 #ifdef USART2_ENABLED
     SET_BIT(RCC->APB1ENR1, RCC_APB1ENR1_USART2EN);
-    usart_setup(USART2, 115200);
-    sw_fifo_init(&usart2_tx_fifo, usart2_tx_buf, USART_BUF_SIZE);
-    sw_fifo_init(&usart2_rx_fifo, usart2_rx_buf, USART_BUF_SIZE);
+    usart_setup(&usart2);
 #endif
 #ifdef USART3_ENABLED
     SET_BIT(RCC->APB1ENR1, RCC_APB1ENR1_USART3EN);
-    usart_setup(USART3, 115200);
-    sw_fifo_init(&usart3_tx_fifo, usart3_tx_buf, USART_BUF_SIZE);
-    sw_fifo_init(&usart3_rx_fifo, usart3_rx_buf, USART_BUF_SIZE);
+    usart_setup(&usart3);
 #endif
 #ifdef USART4_ENABLED
     SET_BIT(RCC->APB1ENR1, RCC_APB1ENR1_UART4EN);
-    usart_setup(USART4, 115200);
-    sw_fifo_init(&usart4_tx_fifo, usart4_tx_buf, USART_BUF_SIZE);
-    sw_fifo_init(&usart4_rx_fifo, usart4_rx_buf, USART_BUF_SIZE);
+    usart_setup(&usart4);
 #endif
 #ifdef USART5_ENABLED
     SET_BIT(RCC->APB1ENR1, RCC_APB1ENR1_UART5EN);
-    usart_setup(USART5, 115200);
-    sw_fifo_init(&usart5_tx_fifo, usart5_tx_buf, USART_BUF_SIZE);
-    sw_fifo_init(&usart5_rx_fifo, usart5_rx_buf, USART_BUF_SIZE);
+    usart_setup(&usart5);
 #endif
 }
 
 /* Setup a given USART port with its baudrate, 8N1 Async Mode, Tx/Rx Interrupt Enabled */
-void usart_setup(USART_TypeDef *usart, uint32_t baudrate)
+void usart_setup(struct usart_handle_t *usart)
 {
+    /* Initialize the FIFO */
+    sw_fifo_init(usart->tx_fifo, usart->tx_buf, USART_BUF_SIZE); 
+    sw_fifo_init(usart->rx_fifo, usart->rx_buf, USART_BUF_SIZE);
+
     /* Baudrate calculation:
      * 
      * Baud = f(CK) / USARTDIV
      * USART gets APB clock and we don't divice APB Clock.
      * i.e. APB clock == System Clock
      */
-    usart->BRR = SystemCoreClock / baudrate;
+    usart->port->BRR = SystemCoreClock / usart->baudrate;
     
     /* 1 stop bit, autobaud disabled, rx timeout disabled
      * DMA disabled, RTS/CTS disabled, IrDA disabled
      */
-    usart->CR3 = 0x0000;
+    usart->port->CR3 = 0x0000;
     /* 8Nx, tx/rx interrupt enabled, tx/rx enabled, enable USART */
-    usart->CR1 = 0x00EC;
+    usart->port->CR1 = 0x00EC;
     /* finally enable the USART */
-    usart->CR1 |= 0x0001;
+    usart->port->CR1 |= 0x0001;
     /* Clear interupts */
-    usart->ICR |= USART_ICR_TCCF;
+    usart->port->ICR |= USART_ICR_TCCF;
 }
 
-void usart_str_send(USART_TypeDef *usart, char *str)
+void usart_str_send(struct usart_handle_t *usart, char *str)
 {
+    /*
+     * If the length of what we're sending is greater than the free space
+     * in the buffer, wait for the buffer to clear up so that we don't lose
+     * any data. The cost in time should be a few milliseconds at most.
+     */
     uint16_t len;
 
     len = strlen(str);
     if (len > 0) {
-        sw_fifo_write(&usart2_tx_fifo, str, strlen(str));
-        SET_BIT(usart->CR1, USART_CR1_TXEIE);
+        if (len >= sw_fifo_get_free_space(usart->tx_fifo)) {
+            while (usart->port->CR1 & USART_CR1_TXEIE);
+        }
+        sw_fifo_write(usart->tx_fifo, str, strlen(str));
+        SET_BIT(usart->port->CR1, USART_CR1_TXEIE);
     }
 }
 
